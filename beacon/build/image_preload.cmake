6d86e473358c8692cb6d958f388fb6c5ddc0ef78
Miktronic <iotclicky@gmail.com>
2023-11-16 04:45:14 -0500

    add hex file

--- beacon/build/image_preload.cmake
+++
@@ -1,8 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "wp_rev1" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "d:/wepowerbeacon/beacon;C:/ncs/v2.4.2/nrf;C:/ncs/v2.4.2/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "d:/WePowerBeacon/beacon/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/31f4403e35/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
